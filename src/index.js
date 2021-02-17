af6bcee7439f5d49df16e6773a5e91359dfc1fd0
Ginkodan <dante.reiter@gmail.com>
2021-02-17 15:43:22 +0100

    dockerfile

--- src/index.js
+++ src/index.js
@@ -1,28 +1,32 @@
+const app = require("express")();
 const { Telegraf, Markup } = require("telegraf");
-require('dotenv').config()
+require("dotenv").config();
 
+//web
+app.get("/", (req, res) => res.json({ message: "Telegram bot" }));
+const port = process.env.PORT || 8080;
+app.listen(port, () =>
+  console.log(`app listening on http://localhost:${port}`)
+);
+
+//telegram bot
 const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.start((ctx) => ctx.reply("Welcome"));
-bot.help((ctx) => ctx.reply("Send me a sticker"));
-bot.on("sticker", (ctx) => ctx.reply("👍"));
-bot.on('callback_query', ctx =>{
-    const plexLib = ctx.update.callback_query.data;
-})
-bot.hears("hi", (ctx) => ctx.reply("Hey there"));
 
+bot.start((ctx) => ctx.reply("Botzilla is booting.."));
 
-bot.command("special", (ctx) => {
-  return ctx.reply(
-    "Special buttons keyboard",
-    Markup.keyboard([
-      Markup.button.contactRequest("Send contact"),
-      Markup.button.locationRequest("Send location"),
-    ]).resize()
-  );
+bot.help((ctx) =>
+  ctx.reply("Following commands are available for now: \n /plex\n /help")
+);
+
+bot.hears("hi", (ctx) => {
+  ctx.reply("Hey there " + ctx.from.first_name);
+});
+
+bot.on("callback_query", (ctx) => {
+  const plexLib = ctx.update.callback_query.data;
 });
 
 bot.command("plex", (ctx) => {
-    
   return ctx.reply(
     "Plex",
 
@@ -33,10 +37,6 @@ bot.command("plex", (ctx) => {
   );
 });
 
-
-function mytest() {
-  return "test";
-}
 function getPlexLib() {
   axios
     .get(`http://192.168.0.123:32400/library/sections`, {
