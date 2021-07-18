f7280cccd7adc58e47958d2a3ec7292b3992a618
andreasvogt89 <30302212+andreasvogt89@users.noreply.github.com>
2021-07-18 21:30:46 +0200

    dockerfile changed to prevent nodemon start in container

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,11 @@
-FROM node:12
+  FROM node:latest
 
-WORKDIR /app
+# Create app directory
+WORKDIR /usr/src/app
 
 COPY package*.json ./
-
 RUN npm install
 
+# Bundle app source
 COPY . .
-
-ENV PORT=8080
-
-EXPOSE 8080
-
-CMD [ "npm", "run", "dev"]
+CMD [ "node", "src/index.js" ]
--- src/public/main.js
+++ src/public/main.js
@@ -1,13 +1,31 @@
 let loading = true;
+
 fetch('/settings').then(response => {
-    return response.json();
-  }).then(result => {
-    document.getElementById("userid").setAttribute('placeholder', result.user_ids);
-    document.getElementById("serie").setAttribute('placeholder', result.serie_folder);
-    document.getElementById("movie").setAttribute('placeholder', result.movie_folder);
-    loading = false;
-    console.log(result)
+  return response.json();
+}).then(result => {
+  document.getElementById("userid").value = result.user_ids;
+  document.getElementById("serie").value = result.serie_folder;
+  document.getElementById("movie").value = result.movie_folder;
+  loading = false;
 });
+let el = document.getElementById("form");
+el.addEventListener("submit", () => {
+  console.log("save clicked")  
+  let settings = {
+      user_ids:document.getElementById("userid").value,
+      serie_folder:document.getElementById("serie").value,
+      movie_folder:document.getElementById("movie").value,
+    };
+   fetch('/settings',{
+    method: 'POST',
+    body: settings 
+   }).then(()=>{
+     alert("Saved!🥳");
+   }).catch(err=>{
+     alert(`Something went wrong😑:\n ${err}`);
+   }); 
+});
+
 
 
 
